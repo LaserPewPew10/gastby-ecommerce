3a757cc5e1b441f6ee776b868f056bc65da2812f
Justin Ramirez <ramirezbjustin@gmail.com>
2021-01-11 17:26:27 -0500

    adding in graphql query for description, images, localfiled, childimagesharp, fluid and src

--- src/templates/ProductTemplate/index.js
+++ src/templates/ProductTemplate/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import {Layout} from 'components';
-import {Grid} from './styles';
+import { Layout } from 'components';
+import { Grid } from './styles';
 
 export const query = graphql`
 query ProductQuery($shopifyId: String){
     shopifyProduct(shopifyId: {eq: $shopifyId}) {
         title
+        description
+        images
+        localfile {
+            childImageSharp {
+                fluid(maxWidth: 300) {
+                    src
+                }
+            }
+        }
     }
 }
 `;
@@ -14,17 +23,18 @@ query ProductQuery($shopifyId: String){
 export default function ProductTemplate(props) {
     console.log(props);
     return (
-<Layout>
-<Grid>
-<div>
-<h1>{props.data.shopifyProduct.title}</h1>
-</div>
-<div>
-image
-</div>
-</Grid>
-</Layout>
+        <Layout>
+            <Grid>
+                <div>
+                    <h1>{props.data.shopifyProduct.title}</h1>
+                    <p>
+                        {props.data.shopifyProduct.description}
+                    </p>
+                </div>
+                <div>
+                    image
+                    </div>
+            </Grid>
+        </Layout>
     );
-    
-    
 }

4e281ad306a36a3bdca3040f8e8d2c999160a98f
Justin Ramirez <ramirezbjustin@gmail.com>
2021-01-13 20:33:22 -0500

    adding changes to cart-content component

--- src/components/CartContents/index.js
+++ src/components/CartContents/index.js
@@ -1,58 +1,74 @@
-import React from ' react';
+import React from 'react';
 import CartContext from 'context/CartContext';
-import {CartItem, CartHeader, CartFooter} from './styles';
-import {QuantityAdjuster} from '../QuantityAdjuster';
-import {RemoveLineItem} from '../RemoveLineItem';
+import { CartItem, CartHeader, CartFooter, Footer } from './styles';
+import { QuantityAdjuster } from '../QuantityAdjuster';
+import { RemoveLineItem } from '../RemoveLineItem';
+import { Button } from '../Button';
+import { navigate } from '@reach/router';
 
+export function CartContents() {
+  const { checkout, updateLineItem } = React.useContext(CartContext);
 
-export function CartContext() {
-    const {checkout, updateLineItem} = React.useContext(CartContext);
+  const handleAdjustQuantity = ({ quantity, variantId }) => {
+    updateLineItem({ quantity, variantId });
+  };
 
-    const handledAdjustQuantity = ({quantity, variantId}) => {
-updateLineItem({quantity, variantId});
-    }
-
-    return (
-  <section>
-      <h1>
-          Your cart
-      </h1>
-      <CartHeader>
-          <div> Product </div>
-          <div> Unit price </div>
-          <div> Quantity </div>
-          <div> Amount </div>
-      </CartHeader>
-      {checkout?checkout.lineItems?checkout.map(item => (
-          <CartItem key={item.variant.id}>
-              <div>
-                  {item.title}
-              </div>
-              <div>
-                  {item.variant.title === 'Default Title' ? '' : item.variant.title}
-              </div>
-              <div>
-                  ${item.variant.price}
-              </div>
-              <div>
-                  <QuantityAdjuster item={item} onAdjust={handledAdjustQuantity}/>
-              </div>
-              <div>
-                  ${(item.quantity * item.variant.price).toFixed(2)}
-              </div>
-              <div>
-                  <RemoveLineItem lineItemId={item.id}/>
-              </div>
-          </CartItem>
+  return (
+    <section>
+      <h1>Your cart</h1>
+      {!!checkout?.lineItems && (
+        <CartHeader>
+          <div>Product</div>
+          <div>Unit price</div>
+          <div>Quantity</div>
+          <div>Amount</div>
+        </CartHeader>
+      )}
+      {checkout?.lineItems?.map(item => (
+        <CartItem key={item.variant.id}>
+          <div>
+            <div>{item.title}</div>
+            <div>
+              {item.variant.title === 'Default Title' ? '' : item.variant.title}
+            </div>
+          </div>
+          <div>£{item.variant.price}</div>
+          <div>
+            <QuantityAdjuster item={item} onAdjust={handleAdjustQuantity} />
+          </div>
+          <div>£{(item.quantity * item.variant.price).toFixed(2)}</div>
+          <div>
+            <RemoveLineItem lineItemId={item.id} />
+          </div>
+        </CartItem>
       ))}
-      <CartFooter>
+      {!!checkout?.lineItems && (
+        <CartFooter>
           <div>
-          <strong>Total:</strong>
+            <strong>Total:</strong>
           </div>
           <div>
-              <span>${checkout?.totalPrice}</span>
+            <span>£{checkout?.totalPrice}</span>
           </div>
-      </CartFooter>
-  </section>
-  )
-};
+        </CartFooter>
+      )}
+      {!checkout?.lineItems && <h4>You cart is empty.</h4>}
+      <Footer>
+        <div>
+          <Button onClick={() => navigate(-1)}>Continue shopping</Button>
+        </div>
+        <div>
+          {!!checkout?.webUrl && (
+            <Button
+              onClick={() => {
+                window.location.href = checkout.webUrl;
+              }}
+            >
+              Checkout
+            </Button>
+          )}
+        </div>
+      </Footer>
+    </section>
+  );
+}

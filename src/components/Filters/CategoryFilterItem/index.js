bab9f1d20b6e435213116456b4158402fceec519
Justin Ramirez <ramirezbjustin@gmail.com>
2021-01-13 20:11:30 -0500

    creating the category filter checkbox functionality

--- src/components/Filters/CategoryFilterItem/index.js
+++ src/components/Filters/CategoryFilterItem/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import {Checkbox} from 'components';
+import { navigate, useLocation } from '@reach/router';
 import {CategoryFilterItemWrapper} from './styles';
+import queryString from 'query-string';
 
-export function CategoryFilterItem({title}) {
-    return (
-        <CategoryFilterItemWrapper><div>{title}</div></CategoryFilterItemWrapper>
-    )
+export function CategoryFilterItem({title, id}) {
+    const { search } = useLocation();
+    const qs = queryString.parse(search);
+    const collectionIds = qs.c?.split(',').filter(c => !!c) || [];
+    const checked = collectionIds?.find(cId => cId === id);
+    const searchTerm = qs.s;
+
+
+
+
+    const onClick = () => {
+        let navigateTo = '/all-products';
+
+        return (
+            <CategoryFilterItemWrapper onClick={onClick}>
+              <Checkbox checked={checked} />
+              <div>{title}</div>
+            </CategoryFilterItemWrapper>
+          );
 }

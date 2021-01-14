83166e87173a5a6c84f83b5178fbc95868d6514a
Justin Ramirez <ramirezbjustin@gmail.com>
2021-01-14 17:11:01 -0500

    adding in SEO

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Layout, CartContents} from 'components';
+import { Layout, CartContents, SEO } from 'components';
 
 export default function CartPage() {
-    return (
-        <Layout>
-        <CartContents/>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <SEO description="The MadHatter cart" title="Cart" />
+      <CartContents />
+    </Layout>
+  );
 }
